b659188a9bed1806a551d75435eee83a8ba93d66
Sujeet17m <sujeet17das@gmail.com>
2025-06-05 19:30:25 +0530

    initial function

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("amount must be greter than 0.")
+
+        else:
+            print("please enter a number")
+    return amount    
